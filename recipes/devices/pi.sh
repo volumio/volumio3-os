a1c2350f5012dd01030d7d59d2a405361b8f8104
Volumio <info@volumio.org>
2022-02-07 17:24:15 +0100

    Kernel 5.10.92 For PI (#35)

--- recipes/devices/pi.sh
+++ recipes/devices/pi.sh
@@ -142,9 +142,10 @@ device_chroot_tweaks_pre() {
 		[5.10.3]="da59cb1161dc7c75727ec5c7636f632c52170961|master|1386"
 		[5.10.73]="1597995e94e7ba3cd8866d249e6df1cf9a790e49|master|1470"
                 [5.10.90]="9a09c1dcd4fae55422085ab6a87cc650e68c4181|master|1512"
+                [5.10.92]="ea9e10e531a301b3df568dccb3c931d52a469106|stable|1514"
 	)
 	# Version we want
-	KERNEL_VERSION="5.4.83"
+	KERNEL_VERSION="5.10.92"
 
 	# For bleeding edge, check what is the latest on offer
 	# Things *might* break, so you are warned!
