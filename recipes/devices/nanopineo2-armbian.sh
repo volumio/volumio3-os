eb1984890d29fcf04955a3508d02f37a486741d5
nikkov <nikkov@gmail.com>
2023-05-31 13:36:16 +0600

    + nanopi* recipes

---
+++ recipes/devices/nanopineo2-armbian.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+DEVICE_SUPPORT_TYPE="C" # First letter (Community Porting|Supported Officially|OEM)
+DEVICE_STATUS="T"       # First letter (Planned|Test|Maintenance)
+
+# Import the NanoPi H5 based family configuration
+# shellcheck source=./recipes/devices/families/nanopi-armbian_h5.sh
+source "${SRC}"/recipes/devices/families/nanopi-armbian_h5.sh
+
+### Device information
+DEVICENAME="NanoPi Neo2" # Pretty name
