c3798e8defbcedf3406035c9e2c1dab5f637ea3b
Gé Koerkamp <ge.koerkamp@gmail.com>
2023-06-07 17:51:14 +0200

    Add support for Radxa Zero 2 (AMLOGIC A311D) and create radxa zero family

--- recipes/devices/radxa-zero2.sh
+++ recipes/devices/radxa-zero2.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-## Setup for Radxa Zero (Amlogic S905Y2)
+## Setup for Radxa Zero 2 (Amlogic A311D)
 DEVICE_SUPPORT_TYPE="C" # First letter (Community Porting|Supported Officially|OEM)
 DEVICE_STATUS="P"       # First letter (Planned|Test|Maintenance)
 
@@ -9,8 +9,5 @@ DEVICE_STATUS="P"       # First letter (Planned|Test|Maintenance)
 source "${SRC}"/recipes/devices/families/radxa-0.sh
 
 ### Device information
-DEVICENAME="Radxa Zero"
-DEVICE="radxa-zero"
-
-
-
+DEVICENAME="Radxa Zero 2"
+DEVICE="radxa-zero2"
