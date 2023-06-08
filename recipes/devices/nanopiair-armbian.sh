4fa5c05c03d67b5ebf94fa6f87b4eb209ae5d86a
Nikolay <nikkov@users.noreply.github.com>
2023-06-08 17:40:07 +0600

    Support devices NanoPi families on armbian base with I2S-slave (#83)
    * + nanopi* recipes
    
    * Fix sysctl.conf overwrite on arm64 images
    
    * added packages
    
    * added packages
    
    * change link to volumio platform repo
    
    * change link to volumio platform repo

---
+++ recipes/devices/nanopiair-armbian.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+DEVICE_SUPPORT_TYPE="C" # First letter (Community Porting|Supported Officially|OEM)
+DEVICE_STATUS="T"       # First letter (Planned|Test|Maintenance)
+
+# Import the NanoPi H3 based family configuration
+# shellcheck source=./recipes/devices/families/nanopi-armbian_h3.sh
+source "${SRC}"/recipes/devices/families/nanopi-armbian_h3.sh
+
+### Device information
+DEVICENAME="NanoPi Neo Air" # Pretty name
