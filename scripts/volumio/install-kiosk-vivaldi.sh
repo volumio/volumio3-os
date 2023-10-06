8c9f938ef6e5c97f734280da4045a0f8dc214503
Volumio <info@volumio.org>
2023-10-06 11:04:09 +0200

    Fix again vivaldi kiosk

--- scripts/volumio/install-kiosk-vivaldi.sh
+++ scripts/volumio/install-kiosk-vivaldi.sh
@@ -63,10 +63,11 @@ fi
 
 
 # Wait for Volumio webUI to be available
-while true; do timeout 2 bash -c '</dev/tcp/127.0.0.1/3000' >/dev/null 2>&1 && break; done
+while [[ $(curl -Is http://localhost:3000 | head -n 1 | cut -d ' ' -f 2) != 200 ]]; do sleep 2; done &
 
 openbox-session & 
-sleep 4 
+#no need for sleep
+#sleep 4 
 
 /usr/bin/vivaldi --kiosk --no-sandbox --disable-background-networking --disable-remote-extensions --disable-pinch --ignore-gpu-blacklist --use-gl=egl --disable-gpu-compositing --enable-gpu-rasterization --enable-zero-copy --disable-smooth-scrolling --enable-scroll-prediction --max-tiles-for-interest-area=512 --num-raster-threads=4 --enable-low-res-tiling --user-agent="volumiokiosk-memorysave-touch" --touch-events --user-data-dir='/data/volumiokiosk' --force-device-scale-factor=1.2 --load-extension='/data/volumiokioskextensions/VirtualKeyboard/' --no-first-run --app=http://localhost:3000 
 " > /opt/volumiokiosk.sh 
