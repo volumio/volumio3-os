8d6eda0a18d579282810ea959778e7abcf8dce4e
Darmur <dario.murgia@alumni-live.uniroma2.it>
2024-01-30 10:36:31 +0100

    add firmware files for brcmfmac43430b0 on pi recipe

--- recipes/devices/pi.sh
+++ recipes/devices/pi.sh
@@ -196,6 +196,7 @@ device_chroot_tweaks_pre() {
 		[TauDAC]="https://github.com/taudac/modules/archive/rpi-volumio-${KERNEL_VERSION}-taudac-modules.tar.gz"
 		[Bassowl]="https://raw.githubusercontent.com/Darmur/bassowl-hat/master/driver/archives/modules-rpi-${KERNEL_VERSION}-bassowl.tar.gz"
 		[wm8960]="https://raw.githubusercontent.com/hftsai256/wm8960-rpi-modules/main/wm8960-modules-rpi-${KERNEL_VERSION}.tar.gz"
+		[brcmfmac43430b0]="https://raw.githubusercontent.com/volumio/volumio3-os-static-assets/master/firmwares/brcmfmac43430b0/brcmfmac43430b0.tar.gz"
 	)
 
 	### Kernel installation
