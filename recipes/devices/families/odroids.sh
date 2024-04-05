741163a308630449cf887364c2374dae4eb92e21
ashthespy <ashthespy@users.noreply.github.com>
2024-04-05 13:03:59 +0200

    Remove legacy/unsupported devices

--- recipes/devices/families/odroids.sh
+++ recipes/devices/families/odroids.sh
@@ -26,8 +26,7 @@ BOOT_START=1
 BOOT_END=64
 BOOT_TYPE=msdos          # msdos or gpt
 BOOT_USE_UUID=yes        # Add UUID to fstab
-INIT_TYPE="initv2" # init{.x86|.nextarm|v2}
-#INIT_TYPE="initv3"
+INIT_TYPE="initv3"
 
 # Modules that will be added to intramsfs
 MODULES=("overlay" "squashfs" "nls_cp437")
@@ -37,6 +36,14 @@ PACKAGES=("lirc" "fbset")
 ### Device customisation
 # Copy the device specific files (Image/DTS/etc..)
 write_device_files() {
+
+  if [ ! -z ${PLYMOUTH_THEME} ]; then
+    log "Plymouth selected, adding plymouth-label to list of packages to install" ""
+    PACKAGES+=("plymouth-label")
+  	log "Copying selected Volumio ${PLYMOUTH_THEME} theme" "cfg"
+    cp -dR "${SRC}/volumio/plymouth/themes/${PLYMOUTH_THEME}" ${ROOTFSMNT}/usr/share/plymouth/themes/${PLYMOUTH_THEME}
+  fi
+
   log "Running write_device_files" "ext"
 
   cp ${PLTDIR}/${DEVICEBASE}/boot/*.ini "${ROOTFSMNT}/boot"
@@ -49,7 +56,7 @@ write_device_files() {
   log "Add additional firmware (mainly wifi)"
   cp -dR "${PLTDIR}/${DEVICEBASE}/firmware" "${ROOTFSMNT}/lib"
 
-  echo "Copying rc.local for ${DEVICENAME} performance tweaks"
+  log "Copying rc.local for ${DEVICENAME} performance tweaks"
   cp "${PLTDIR}/${DEVICEBASE}/etc/rc.local" "${ROOTFSMNT}/etc"
 
   log "Copying LIRC configuration files for HK stock remote"
@@ -74,8 +81,23 @@ device_image_tweaks() {
 device_chroot_tweaks_pre() {
   log "Performing device_chroot_tweaks_pre" "ext"
 
-  log "Creating boot.ini from template"
+  log "Creating UUIDs in boot.ini" "cfg"
+  sed -i "s/bootconfig/uuidconfig/" /boot/boot.ini
   sed -i "s/%%VOLUMIO-PARAMS%%/imgpart=UUID=${UUID_IMG} bootpart=UUID=${UUID_BOOT} datapart=UUID=${UUID_DATA}/" /boot/boot.ini
+
+  # Configure kernel parameters, overrule $verbosity in order to keep the template (platform files) untouched
+  if [ "${DEBUG_IMAGE}" == "yes" ]; then
+    log "Configuring DEBUG kernel parameters" "cfg"
+    sed -i "s/loglevel=\$verbosity/loglevel=8 nosplash break= use_kmsg=yes/" /boot/boot.ini
+  else
+    log "Configuring default kernel parameters" "cfg"
+    sed -i "s/loglevel=\$verbosity/quiet loglevel=0/" /boot/boot.ini
+    if [ ! -z "${PLYMOUTH_THEME}" ]; then
+      log "Adding splash kernel parameters" "cfg"
+      plymouth-set-default-theme -R ${PLYMOUTH_THEME}
+      sed -i "s/loglevel=0/loglevel=0 splash plymouth.ignore-serial-consoles initramfs.clear/" /boot/boot.ini
+    fi  
+  fi
 
   log "Fixing armv8 deprecated instruction emulation with armv7 rootfs"
   cat <<-EOF >>/etc/sysctl.conf
