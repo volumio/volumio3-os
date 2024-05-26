0daa1222808831b7ac6c926e3ac8fd45e1b467d2
foonerd <andrew@dt-ltd.net>
2024-05-26 13:03:57 +0100

    mpd.socket is owned by pid 1 - no way to manage it.

--- volumio/bin/mpd-wrapper.sh
+++ volumio/bin/mpd-wrapper.sh
@@ -10,10 +10,6 @@ sleep 2;
 # Find PID of mpd process
 MPDPID=`ps -eaf | grep mpd | grep -v grep | awk '{print $2}'`
 
-# Find PID of mpd.socket
-PORT=6600
-SOCPID=$(/usr/bin/lsof -ti:$PORT)
-
 # mpd process left abandon and unit file is not able to stop it
 
 if ! [[ "$MPDPID" = ^[0-9]+$ ]] ;
@@ -25,18 +21,6 @@ fi
 if [[ "" !=  "$MPDPID" ]]; then
   printf "Killing abandon mpd under PID $MPDPID\n"
   /usr/bin/sudo /bin/kill -9 $MPDPID
-fi
-
-# mpd socket left abandon and unit file is not able to stop it
-if ! [[ "$SOCPID" = ^[0-9]+$ ]] ;
-then
-  printf "No mpd socket process found, exiting.\n"
-  exit 0
-fi
-
-if [[ "" !=  "SOCPID" ]]; then
-  printf "Killing mpd socket process PID $SOCPID running on port: $PORT\n"
-  /usr/bin/sudo /bin/kill -9 $SOCPID
 fi
 
 # start mpd unit systemd service
