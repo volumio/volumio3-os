4a04d69b3c895a734e52218c68cd98e3f538eb98
Darmur <dario.murgia@alumni-live.uniroma2.it>
2024-06-27 12:06:06 +0200

    revert pi kernel to 6.1.69

--- recipes/devices/pi.sh
+++ recipes/devices/pi.sh
@@ -196,7 +196,7 @@ device_chroot_tweaks_pre() {
 		[6.1.77]="5fc4f643d2e9c5aa972828705a902d184527ae3f|master|1730"
 	)
 	# Version we want
-	KERNEL_VERSION="6.1.77"
+	KERNEL_VERSION="6.1.69"
 
 	MAJOR_VERSION=$(echo "$KERNEL_VERSION" | cut -d '.' -f 1)
 	MINOR_VERSION=$(echo "$KERNEL_VERSION" | cut -d '.' -f 2)
