147953990fca4aa103a8f90538ef56fb0454638c
Volumio <info@volumio.org>
2025-02-26 16:24:26 +0000

    Increase checking timeout of MPD

--- volumio/bin/mpd_monitor.sh
+++ volumio/bin/mpd_monitor.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # Check interval (in seconds)
-CHECK_INTERVAL=10
+CHECK_INTERVAL=20
 # Define the timeout (in seconds) for no output
-TIMEOUT=2
+TIMEOUT=10
 
+echo "MPD Monitor Service: Starting MPD Monitor Service"
 while true; do
     # Get output of mpc command and measure time it takes to respond
     OUTPUT=$(timeout $TIMEOUT mpc)
     
     if [ -z "$OUTPUT" ]; then
         # If there is no output, restart the MPD service
+	echo "MPD Monitor Service: MPD Appears to be inactive, restarting" 
         killall -g mpd
         systemctl restart mpd.service
-        echo "MPD restarted due to no mpc output."
+        echo "MPD Monitor Service: MPD restarted due to no mpc output."
     fi
 
     # Wait for the defined interval before checking again
