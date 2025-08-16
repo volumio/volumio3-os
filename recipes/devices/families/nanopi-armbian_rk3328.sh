7044262daed40f016564d0bf019f582e0bb49d71
Darmur <dario.murgia@alumni-live.uniroma2.it>
2025-08-16 11:07:51 +0200

    fix for nanopi armbian recipes, download device-tree-compiler from archive ftp

--- recipes/devices/families/nanopi-armbian_rk3328.sh
+++ recipes/devices/families/nanopi-armbian_rk3328.sh
@@ -72,7 +72,7 @@ device_chroot_tweaks_pre() {
   log "Performing device_chroot_tweaks_pre" "ext"
 
   echo "Install device tree compiler with overlays support"
-  wget -P /tmp http://ftp.debian.org/debian/pool/main/d/device-tree-compiler/device-tree-compiler_1.4.7-4_armhf.deb
+  wget -P /tmp http://archive.debian.org/debian/pool/main/d/device-tree-compiler/device-tree-compiler_1.4.7-4_armhf.deb
   dpkg -i /tmp/device-tree-compiler_1.4.7-4_armhf.deb
   rm /tmp/device-tree-compiler_1.4.7-4_armhf.deb
 
